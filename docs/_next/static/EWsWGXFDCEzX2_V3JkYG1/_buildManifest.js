abdcdb4947a8f6f29be06b1edd747ad41e058fee
YossiSuper <yossisuper1023@gmail.com>
2023-01-01 13:34:35 +0900

    first comit

---
+++ docs/_next/static/EWsWGXFDCEzX2_V3JkYG1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1beb14451460885a.css","static/chunks/pages/index-43e760ad3f8c0a86.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
